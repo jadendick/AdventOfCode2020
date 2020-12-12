5ababa4a0ad1d5e60dd062a11c3da18363b6725b
jadendick <45302482+jadendick@users.noreply.github.com>
2020-12-11 23:49:22 -0600

    Day 12

--- day12/code.py
+++ day12/code.py
@@ -2,14 +2,87 @@ file = open("input","r")
 input = file.read().split("\n")[:-1]
 
 def part1():
-    sum = 0
-    
-    return sum
+    north = 0
+    east = 0
+    facing = 1
+    for x in input:
+        dir = x[0]
+        mag = int(x[1:])
+
+        if(dir == "N"):
+            north += mag
+        elif(dir == "S"):
+            north -= mag
+        elif(dir == "E"):
+            east += mag
+        elif(dir == "W"):
+            east -= mag
+        elif(dir == "F"):
+            if(facing == 0):
+                north += mag
+            elif(facing == 1):
+                east += mag
+            elif(facing == 2):
+                north -= mag
+            elif(facing == 3):
+                east -= mag
+        elif(dir == "R"):
+            facing = (facing + mag/90) % 4
+        elif(dir == "L"):
+            facing = (facing - mag/90) % 4
+            
+    return abs(east) + abs(north)
+
 
 def part2():
-    sum = 0
+    north = 0
+    east = 0
+    wnorth = 1
+    weast = 10
+    for x in input:
+        dir = x[0]
+        mag = int(x[1:])
 
-    return sum
+        if(dir == "N"):
+            wnorth += mag
+        elif(dir == "S"):
+            wnorth -= mag
+        elif(dir == "E"):
+            weast += mag
+        elif(dir == "W"):
+            weast -= mag
+        elif(dir == "F"):
+            north += wnorth * mag
+            east += weast * mag
+        elif(dir == "R"):
+            deg = mag/90 % 4
+            if(deg == 1):
+                onorth = wnorth
+                wnorth = -weast
+                weast = onorth
+            elif(deg == 2):
+                wnorth = -wnorth
+                weast = -weast
+            elif(deg == 3):
+                onorth = wnorth
+                wnorth = weast
+                weast = -onorth
+            
+        elif(dir == "L"):
+            deg = mag/90 % 4
+            if(deg == 1):
+                onorth = wnorth
+                wnorth = weast
+                weast = -onorth
+            elif(deg == 2):
+                wnorth = -wnorth
+                weast = -weast
+            elif(deg == 3):
+                onorth = wnorth
+                wnorth = -weast
+                weast = onorth
+            
+    return abs(east) + abs(north)
 
 print("Part 1: ",part1())
 print("Part 2: ",part2())
